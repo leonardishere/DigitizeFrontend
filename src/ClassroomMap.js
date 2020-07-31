4c8aa795498db869d16971fc0e654fad3b4369cf
Andrew Leonard <leonardishere@gmail.com>
2020-07-31 06:10:42 -0700

    convert checked in students to happy little svgs

--- src/ClassroomMap.js
+++ src/ClassroomMap.js
@@ -51,32 +51,43 @@ class ClassroomMap extends React.Component {
           <img src={picture} alt="A map of Kenna 104" className="background_img"/>
           <div className="overlay">
             <svg viewBox="0 0 612 320" id="svg">
-              {
-                desk_paths.map((path,i) => <path d={path} key={i}></path>)
-              }
-              {
-                cardreader_statuses.map((status,i)=>{
-                  if(status.status === 'unoccupied') return (
-                    <circle
-                      key={status.cardreaderid}
-                      cx={chair_coords[i][0]}
-                      cy={chair_coords[i][1]}
-                      r="10" stroke="black" strokeWidth="1" fill="white"
-                    />
-                  )
-                  else return (
-                    <circle
-                      key={status.cardreaderid}
-                      cx={chair_coords[i][0]}
-                      cy={chair_coords[i][1]}
-                      r="10" stroke="black" strokeWidth="1"
-                      fill={ status.status === 'occupied' ? 'green' : 'blue' }
-                      onMouseEnter={e=>this.props.handleHover(status.cardreaderid,true)}
-                      onMouseLeave={e=>this.props.handleHover(status.cardreaderid,false)}
-                    />
-                  )
-                })
-              }
+              <g className="desks">
+                {
+                  desk_paths.map((path,i) => <path d={path} key={i}></path>)
+                }
+              </g>
+              <g className="chairs">
+                {
+                  cardreader_statuses.map((status,i)=>{
+                    if(status.status === 'unoccupied') return (
+                      <circle
+                        key={status.cardreaderid}
+                        cx={chair_coords[i][0]}
+                        cy={chair_coords[i][1]}
+                        r="10" stroke="black" strokeWidth="1" fill="white"
+                      />
+                    )
+                    else {
+                      var transform = "translate(" + (chair_coords[i][0]-10) + " " + (chair_coords[i][1]-10) + ") scale(0.0403 0.0391)"
+                      return (
+                        <g
+                          key={i}
+                          onMouseEnter={e=>this.props.handleHover(status.cardreaderid,true)}
+                          onMouseLeave={e=>this.props.handleHover(status.cardreaderid,false)}>
+                          <circle
+                            key={status.cardreaderid}
+                            cx={chair_coords[i][0]}
+                            cy={chair_coords[i][1]}
+                            r="9" stroke="black" strokeWidth="1"
+                            fill={ status.status === 'occupied' ? 'yellow' : 'green' }
+                          />
+                          <path transform={transform} d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 448c-110.3 0-200-89.7-200-200S137.7 56 248 56s200 89.7 200 200-89.7 200-200 200zm105.6-151.4c-25.9 8.3-64.4 13.1-105.6 13.1s-79.6-4.8-105.6-13.1c-9.8-3.1-19.4 5.3-17.7 15.3 7.9 47.1 71.3 80 123.3 80s115.3-32.9 123.3-80c1.6-9.8-7.7-18.4-17.7-15.3zm-235.9-72.9c3.5 1.1 7.4-.5 9.3-3.7l9.5-17c7.7-13.7 19.2-21.6 31.5-21.6s23.8 7.9 31.5 21.6l9.5 17c2.1 3.7 6.2 4.7 9.3 3.7 3.6-1.1 6-4.5 5.7-8.3-3.3-42.1-32.2-71.4-56-71.4s-52.7 29.3-56 71.4c-.3 3.7 2.1 7.2 5.7 8.3zm160 0c3.5 1.1 7.4-.5 9.3-3.7l9.5-17c7.7-13.7 19.2-21.6 31.5-21.6s23.8 7.9 31.5 21.6l9.5 17c2.1 3.7 6.2 4.7 9.3 3.7 3.6-1.1 6-4.5 5.7-8.3-3.3-42.1-32.2-71.4-56-71.4s-52.7 29.3-56 71.4c-.3 3.7 2.1 7.2 5.7 8.3z"></path>
+                        </g>
+                      )
+                    }
+                  })
+                }
+              </g>
             </svg>
           </div>
         </CardBody>
